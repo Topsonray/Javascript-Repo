9a22be2b55cd5d4c7f4a4ad641c37196bb917c6c
Topsonray <stopsonray@gmail.com>
2024-09-02 17:12:27 +0100

    first second javascript assignment

---
+++ App.js
@@ -0,0 +1,18 @@
+// In this challenge, you are going to create a function that tells us how many days, weeks and months we have left if we live until 90 years old. It will take your current age as the input and console.logs a message with our time left in this format: You have x days, y weeks, and z months left.
+
+function lifeInWeeks(age) {
+   
+/************Don't change the code above************/    
+   
+    //Write your code here.
+    var age=alert("Enter Your Current Age?");
+    daysLeft=(90-age)*365;
+    weeksLeft=(90-age)*52;
+    yearLeft=(90-age)*12;
+    console.log("You have:" +daysLeft +'days' + ',' +weeksLeft +'weeks'+ ',' +monthsLeft +'months'+'left');
+   
+   
+   
+   
+/*************Don't change the code below**********/
+}
